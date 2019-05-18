32743c8dae637ae13eecd22774b0bcde895c8aed
orgaborr <45071962+orgaborr@users.noreply.github.com>
2019-05-19 01:03:43 +0200

    fibonacci(int n) method added

--- Series.java
+++ Series.java
@@ -11,6 +11,7 @@ public class Series {
           return sum;
    }
   
+//multiplies all numbers up to n, 0 factorial is 1 by default
    public int factorial(int n) {
 	    if(n == 0) {
 	        return 1;
@@ -22,7 +23,21 @@ public class Series {
 	    }
 	    
 	    return fact;
-	 }
+   }
+//the next number is always the sum of the 2 previous ones, starts with 0, 1, 1, 2, ...	
+public int fibonacci(int n) {
+        int[] fibonacciArray = new int[n+2]; //enables the initiation of the first 2 numbers
+        fibonacciArray[0] = 0;
+        fibonacciArray[1] = 1;
+        
+        if(n>1) {
+            for(int i=2; i<=fibonacciArray.length-2; i++) { //length-2 to balance the extension at initialization of the array
+                fibonacciArray[i] = fibonacciArray[i-1] + fibonacciArray[i-2];
+            }
+        }
+        
+        return fibonacciArray[n];
+    }
   
 }
 
