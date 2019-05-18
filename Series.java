1ee4afb54ed469adbf7875e5720cd304d526bc92
orgaborr <45071962+orgaborr@users.noreply.github.com>
2019-05-19 00:15:27 +0200

    factorial(int n) method added

--- Series.java
+++ Series.java
@@ -1,7 +1,7 @@
 public class Series {
 
   //returns the sum of all integers up to n (inclusive)
-  public static int nSum(int n) {
+  public int nSum(int n) {
           int sum = 0;
           
           for(int i=0; i<=n; i++) {
@@ -9,5 +9,20 @@ public class Series {
           }
         
           return sum;
-      }
- }
+   }
+  
+   public int factorial(int n) {
+	    if(n == 0) {
+	        return 1;
+	    }
+	    
+	    int fact = 1;
+	    for(int i=1; i<=n; i++) {
+	        fact *= i;
+	    }
+	    
+	    return fact;
+	 }
+  
+}
+
