294089ca6b9cf218086b676f9c0a6be6d70617ff
Gabor <orbangabeszb@gmail.com>
2019-05-19 16:12:51 +0200

    first commit

---
+++ src/com/orgabor/myLibraries/Series.java
@@ -0,0 +1,44 @@
+package com.orgabor.myLibraries;
+
+public class Series {
+
+	//returns the sum of all integers up to n (inclusive)
+	public static int nSum(int n) {
+		int sum = 0;
+
+		for(int i=0; i<=n; i++) {
+			sum += i;
+		}
+
+		return sum;
+	}
+
+	//multiplies all numbers up to n, 0 factorial is 1 by default
+	public static int factorial(int n) {
+		if(n == 0) {
+			return 1;
+		}
+
+		int fact = 1;
+		for(int i=1; i<=n; i++) {
+			fact *= i;
+		}
+
+		return fact;
+	}
+	//the next number is always the sum of the 2 previous ones, starts with 0, 1, 1, 2, ...	
+	public static int fibonacci(int n) {
+		int[] fibonacciArray = new int[n+2]; //enables the initiation of the first 2 numbers
+		fibonacciArray[0] = 0;
+		fibonacciArray[1] = 1;
+
+		if(n>1) {
+			for(int i=2; i<=fibonacciArray.length-2; i++) { 					//length-2 to balance the extension																	
+				fibonacciArray[i] = fibonacciArray[i-1] + fibonacciArray[i-2]; 	//at initialization of the array
+			}
+		}
+
+		return fibonacciArray[n];
+	}
+
+}
